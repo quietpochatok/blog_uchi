1c777b2aa6e751242289ad81ca3d6cd2cfa9570e
quietpochatok <rabochii53@gmail.com>
2021-04-11 16:45:01 +0500

    add jquery and bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,6 +1,8 @@
 require("@rails/ujs").start()
 // require("@rails/activestorage").start()
 // require("channels")
+import 'bootstrap/dist/js/bootstrap'
+
 import '../styles/application'
 const images = require.context('../images', true)
 
