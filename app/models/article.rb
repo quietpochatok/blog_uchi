7a5e1fb8597f134fcf5cb3067a7f7e10da924386
quietpochatok <rabochii53@gmail.com>
2021-04-12 00:24:08 +0500

    Add polymorphic model Comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_180643) do
+ActiveRecord::Schema.define(version: 2021_04_11_191814) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_04_11_180643) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["title"], name: "index_articles_on_title", unique: true
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "users", force: :cascade do |t|
