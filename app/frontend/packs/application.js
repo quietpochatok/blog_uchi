82b3468bd9ab41c2f088e2d0cb05626a2df9ac8d
quietpochatok <rabochii53@gmail.com>
2021-04-11 16:05:23 +0500

    init app

---
+++ app/frontend/packs/application.js
@@ -0,0 +1,6 @@
+require("@rails/ujs").start()
+// require("@rails/activestorage").start()
+// require("channels")
+import '../styles/application'
+const images = require.context('../images', true)
+
